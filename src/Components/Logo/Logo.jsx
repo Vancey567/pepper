751e1b452ac050488cfe91d3270ff1e241f5dde4
Jay Mishra <66526393+Vancey567@users.noreply.github.com>
2023-02-08 18:03:27 +0530

    Update Logo.jsx

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './Logo.css';
 
 import footerLogo from "../../images/footer-logo.png";
+import logoImg from "../../images/logo.png";
 
 const url = process.env.REACT_APP_BASE_URL || 'http://localhost:5000';
 
@@ -23,7 +24,8 @@ const Logo = ({section}) => {
 
   return (
     <div>
-      <img className='logo' src={section === 'footer' ?  footerLogo : logo} alt="logo"/>
+      // <img className='logo' src={section === 'footer' ?  footerLogo : logo} alt="logo"/>
+      <img className='logo' src={logoImg} alt="logo"/>
     </div>
   )
 }
