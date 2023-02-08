f3ed362b8edc242a117b9032ff7c1f36b8265f92
Jay Mishra <66526393+Vancey567@users.noreply.github.com>
2023-02-08 18:06:57 +0530

    Update Logo.jsx

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -7,25 +7,9 @@ import logoImg from "../../images/logo.png";
 const url = process.env.REACT_APP_BASE_URL || 'http://localhost:5000';
 
 const Logo = ({section}) => {
-  const [logo, setLogo] = useState('')
-
-  useEffect(() => {
-    const readData = async () => {
-      try {
-        const res = await fetch(`${url}/admin/data`);
-        const data = await res.json();
-        setLogo(url + data.image);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    readData();
-  }, [logo]);
-
   return (
     <div>
-      // <img className='logo' src={section === 'footer' ?  footerLogo : logo} alt="logo"/>
-      <img className='logo' src={logoImg} alt="logo"/>
+      <img className='logo' src={section === 'footer' ?  footerLogo : logoImg} alt="logo"/>
     </div>
   )
 }
